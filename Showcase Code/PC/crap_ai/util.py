3e9da70458b5823dcdbe2354ec30caa8e2429b21
b-kirk <146019578+b-kirk@users.noreply.github.com>
2023-10-11 16:04:17 +0100

    Update util.py

--- Showcase Code/PC/crap_ai/util.py
+++ Showcase Code/PC/crap_ai/util.py
@@ -21,17 +21,17 @@ def convert_to_fen(board): # Converts to Forsyth–Edwards Notation such that th
     } # Dictionary of values such that each piece on the server board 
 
     fen = ""
-    for x in range(8):
+    for x in range(8): # Iterates through each row 
         count = 0
-        for y in range(8):
-            if board[x][y] in ("..", "--", "??"):
+        for y in range(8): # Iterates through each column piece in the current row 
+            if board[x][y] in ("..", "--", "??"): # If board has any of these values (not represented as pieces) it will add one to the count, which is added to the string in accordance to FEN notation
                 count += 1
             elif count > 0:
-                fen += str(count)
-                fen += peice_conversion.get(board[x][y])
-                count = 0
+                fen += str(count) # Once a represented piece is reached, add the number of empty spaces to the fen string
+                fen += piece_conversion.get(board[x][y]) # Add the piece conversion representation to the string.
+                count = 0 # Resets count to begin counting again
             else:
-                fen += peice_conversion.get(board[x][y])
+                fen += piece_conversion.get(board[x][y]) # If there are no empty spaces, places the next piece 
         if count > 0:
             fen += str(count)
         if x != 7:
