639c5d0216c42ae5a87a679426a681dd3f3ef22b
h57819dw <dylan.williamson@student.manchester.ac.uk>
2023-10-18 14:22:32 +0100

    Fix piece name in declaration

--- Showcase Code/PC/crap_ai/util.py
+++ Showcase Code/PC/crap_ai/util.py
@@ -5,7 +5,7 @@ DEFAULT_WEBSERVER = "http://127.0.0.1:5000" # Default port for localhost
 
 
 def convert_to_fen(board): # Converts to Forsyth–Edwards Notation such that the read piece is equal to the piece name in FEN, FEN Notation: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
-    peice_conversion = {
+    piece_conversion = {
         "BK": "k",
         "BQ": "q",
         "BB": "b",
