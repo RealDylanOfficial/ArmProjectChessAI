3e1ec696dcee0386450dde35e5e696ab9639605a
h57819dw <dylan.williamson@student.manchester.ac.uk>
2023-11-08 15:40:52 +0000

    Started test for AI

---
+++ Showcase Code/PC/crap_ai/test.py
@@ -0,0 +1,13 @@
+from . import ai, main, server, tables, util
+
+board = bytes(
+
+ """BR BN BB BQ BK BB BR BR
+    BP BP BP BP BP BP BP BP
+    .. .. .. .. .. .. .. ..
+    .. .. .. .. .. .. .. ..
+    .. .. .. .. .. .. .. ..
+    .. .. .. .. .. .. .. ..
+    WP WP WP WP WP WP WP WP
+    WR WN WB WQ WK WB WN WR"""
+      , 'utf8')
