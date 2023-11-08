5d0fc869e2c0858ace890c3705d6c5c46db4be3b
Ben Kirk <146019578+b-kirk@users.noreply.github.com>
2023-11-08 15:58:31 +0000

    Update __init__.py

--- Showcase Code/PC/crap_ai/__init__.py
+++ Showcase Code/PC/crap_ai/__init__.py
@@ -9,7 +9,7 @@ alphabeta_ai = ai.AI_AlphaBeta()
 
 
 def get_move(url, depth=3):
-    board = chess.Board(convert_to_fen(read_board_network(url)))
+    board = chess.Board(convert_to_fen(read_board_network(url))) # Instantiates board via reading (using read_board_network) and the converting to fen, putting into chess.py board ? WORKING ON 
 
     start_time = time.time()
     ai_move = alphabeta_ai.get_move(board, depth)
